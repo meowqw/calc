762753320d54a27c73ec65038b9c93a2533d6a8b
olega211 <106370448+olega211@users.noreply.github.com>
2023-08-20 13:48:32 +0700

    feat: убрал ограничение на вызовы

--- backend/app/Providers/RouteServiceProvider.php
+++ backend/app/Providers/RouteServiceProvider.php
@@ -37,7 +37,7 @@ class RouteServiceProvider extends ServiceProvider
     {
         $this->routes(function () {
             Route::prefix('api/v1')
-                ->middleware('api')
+//                ->middleware('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
